256856cc74ceeedffc408102331ec0ae75ef0a58
DanuxPeach <21520682@gm.uit.edu.vn>
2025-02-05 18:32:56 +0700

    upload code FileUpload web

---
+++ FileUpload/web/php/upload.php
@@ -0,0 +1,21 @@
+<?php
+
+define( 'DVWA_WEB_PAGE_TO_ROOT', '/' );
+
+if( isset( $_POST[ 'Upload' ] ) ) {
+    // Where are we going to be writing to?
+    $target_path  = DVWA_WEB_PAGE_TO_ROOT . "uploads/";
+    $target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
+
+    // Can we move the file to the upload folder?
+    if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
+        // No
+        echo '<pre>Your image was not uploaded.</pre>';
+    }
+    else {
+        // Yes!
+        echo "<pre>{$target_path} succesfully uploaded!</pre>";
+    }
+}
+
+?>
